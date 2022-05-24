15addf2e191acad945e6baf0b6bdf8ec1d420127
Deepak Bawa <deepak.bawa@orionesolutions.in>
2022-05-24 17:57:22 +0530

    added localstorage

--- src/app/user/store/user.reducer.ts
+++ src/app/user/store/user.reducer.ts
@@ -2,11 +2,16 @@ import { createReducer, on } from '@ngrx/store';
 import { User } from "../userInterface";
 import { addUser } from './user.actions';
 
-export const initialState: User[] = [];
+let getUsrStr: string = localStorage.getItem('users') || ''
+let data: User[] = getUsrStr ? JSON.parse(getUsrStr) : []
+
+export const initialState: User[] = data;
 
 export const userReducer = createReducer(
   initialState,
   on(addUser, (oldState, newState ) => {
-    return [...oldState, {...newState.user}]
+    let user = [...oldState, {...newState.user}]
+    localStorage.setItem('users', JSON.stringify(user))
+    return user
   }),
 );
